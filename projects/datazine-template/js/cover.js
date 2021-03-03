2ff3d993f5eafb495dfa73346d7e56e396d6436d
Sarah <sa4913@nyu.edu>
2021-03-03 10:09:41 +0800

    added path for World

--- projects/datazine-template/js/cover.js
+++ projects/datazine-template/js/cover.js
@@ -10,17 +10,17 @@ viz.append("circle") //Earth base
   .attr("r", 400)
   .attr("fill", "lightblue")
 ;
-viz.append("path") //attempt an arc
+viz.append("path") //attempt an arc for Hello
 //M 0,300 A 200,200 0 0,1 400,300
   .attr("id", "helloPath")
   .attr("d", "M 350,350 A 300,300, 0, 0,1 750,125")
   .style("fill", "none")
   .style("stroke", "none")
 ;
-viz.append("path") //attempt an arc
+viz.append("path") //attempt an arc for World
 //M 0,300 A 200,200 0 0,1 400,300
   .attr("id", "worldPath")
-  .attr("d", "M 350,350 A 300,300, 0, 0,1 750,125")
+  .attr("d", "M 500,700 A 300,300, 0, 0,0 900,475")
   .style("fill", "none")
   .style("stroke", "none")
 ;
@@ -34,6 +34,16 @@ viz.append("text") //Hello
     .attr("fill", "blue")
 
 ;
+viz.append("text") //Hello
+  .append("textPath")
+    .attr("xlink:href", "#worldPath")
+    .style("text-anchor", "beginning")
+    .attr("startOffset", "25%")
+    .text("World")
+    .attr("font-size", 100)
+    .attr("fill", "blue")
+
+;
 // viz.append("text") //Hello
 //   .text("Hello")
 //   .attr("font-size", 150)
@@ -41,13 +51,13 @@ viz.append("text") //Hello
 //   .attr("x", 300)
 //   .attr("y", 300)
 // ;
-viz.append("text") // World
-  .text("World")
-  .attr("font-size", 150)
-  .attr("fill", "blue")
-  .attr("x", 500)
-  .attr("y", 600)
-;
+// viz.append("text") // World
+//   .text("World")
+//   .attr("font-size", 150)
+//   .attr("fill", "blue")
+//   .attr("x", 500)
+//   .attr("y", 600)
+// ;
 viz.append("text") //Greetings
   .text("3 Weeks of Greetings")
   .attr("font-size", 50)
