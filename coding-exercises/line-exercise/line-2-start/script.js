25ded755d0f71feffbff450dfa269b8f235bea64
Sarah <sa4913@nyu.edu>
2021-03-30 22:13:48 +0800

    favicon update

--- coding-exercises/line-exercise/line-2-start/script.js
+++ coding-exercises/line-exercise/line-2-start/script.js
@@ -21,9 +21,9 @@ function gotData(incomingData){
   incomingData = fixJSDateObjects(incomingData);
   console.log(incomingData);
 
-
   // temporarily flatten data to get the minima/maxima:
   let flatData = d3.merge(incomingData)
+  console.log(flatData);
   // we can use a  time scale because our data expresses
   // years in the form of JS date objects
   let xDomain = d3.extent(flatData, function(d){ return d.year });
@@ -47,9 +47,95 @@ function gotData(incomingData){
   ;
   yAxisGroup.call(yAxis);
 
-
   let graphGroup = viz.append("g").attr("class", "graphGroup");
 
+  let dataIndex = 0;
+
+  //we have to tell the line function what data to look at
+  let lineMaker = d3.line()
+    .x(function(d, i){
+      return xScale(d.year);
+    })
+    .y(function(d, i){
+      return yScale(d.birthsPerThousand);
+    })
+  ;
+
+  // graphGroup.selectAll(".line").data(incomingData).enter()
+  //   .append("path")
+  //     .attr("d", lineMaker)
+  //     .attr("fill", "none")
+  //     .attr("stroke", "black")
+  //     .attr("stroke-width", 5)
+  //     .attr("class", "line")
+  //     .attr("stroke", function(d, i){
+  //       if (d[0].country == "United States") {
+  //         return "blue"
+  //       } else {
+  //         return "red"
+  //       }
+  //     })
+  // ;
+
+  function showByCountry(){
+
+    let dataToShow = [incomingData[dataIndex]];
+    console.log("dataToShow", dataToShow);
+
+    function assignKeys(d, i){
+      return d.year
+    }
+
+    let dataLine = graphGroup.selectAll(".line").data(dataToShow, assignKeys);
+
+    //ENTER
+    dataLine.enter()
+      .append("path")
+        .attr("class", "line")
+        .attr("d", lineMaker)
+        .attr("fill", "none")
+        .attr("stroke-width", 5)
+        .attr("class", "line")
+        .attr("stroke", function(d, i){
+          if (d[0].country == "United States") {
+
+            return "blue"
+          } else {
+            return "red"
+          }
+        })
+    ;
+
+    //We don't need to exit the line
+    //because both usa and china have data for the same years
+    //we are just updating the data from one to the other
+
+    //UPDATE
+    //update the path and color
+    dataLine.transition()
+      .attr("d", lineMaker)
+      .attr("stroke", function(d, i){
+        if (d[0].country == "United States") {
+          return "blue"
+        } else {
+          return "red"
+        }
+      })
+      .duration(500)
+    ;
+
+  }
+
+  document.getElementById("usa").addEventListener("click", function(){
+    dataIndex = 0;
+    document.getElementById("favicon").href = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Seal_of_the_United_States_Department_of_State.svg/1024px-Seal_of_the_United_States_Department_of_State.svg.png";
+    showByCountry();
+  });
+  document.getElementById("china").addEventListener("click", function(){
+    dataIndex = 1;
+    document.getElementById("favicon").href = "https://www.kindpng.com/picc/m/106-1065813_china-flag-round-png-transparent-png.png";
+    showByCountry();
+  });
 
 }
 
